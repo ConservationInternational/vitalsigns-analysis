a84fdce6c68aabd073fd478a52c4c03d870d501f
mcooperci <mw.coop.r@gmail.com>
2017-01-29 23:51:16 +0300

    Finish first draft of getting all relevant vars into one df

--- AgIntensification_Livelihoods/AgIntensification_Livelihoods.R
+++ AgIntensification_Livelihoods/AgIntensification_Livelihoods.R
@@ -23,7 +23,7 @@ flagFilter <- function(df){
 #Independant Vars indicative of Ag Intensification
 #####################################################
 
-iv_df <- tbl(con, "flagging__agric") %>%
+allvars_df <- tbl(con, "flagging__agric") %>%
   select(survey_uuid, Country, `Landscape #`, `Household ID`, flag) %>%
   data.frame %>%
   filter(!grepl('record is duplicated in the schema', flag)) %>%
@@ -39,7 +39,7 @@ field_size <- tbl(con, "flagging__agric_field_roster") %>%
   group_by(survey_uuid) %>%
   summarize(average_field_size = mean(ag2a_04, na.rm=T), total_acreage = sum(ag2a_04, na.rm=T))
 
-iv_df <- merge(iv_df, field_size, all.x=T)
+allvars_df <- merge(allvars_df, field_size, all.x=T)
 
 
 # Intercropping  - agric_crops_by_field
@@ -55,7 +55,7 @@ intercropping$ag4a_04[intercropping$ag4a_04==2] <- 0
 intercropping <- intercropping %>% group_by(survey_uuid) %>%
   summarize(intercrop_rate = mean(ag4a_04, na.rm=T))
 
-iv_df <- merge(iv_df, intercropping, all.x=T)
+allvars_df <- merge(allvars_df, intercropping, all.x=T)
 
 # Inputs - agric_field_details
 #  ag3a_34 - What was the main type of pesticide/herbicide that you applied? {1: 'Pesticide', 2: 'Herbicide', 3: 'Fungicide'}
@@ -96,7 +96,7 @@ inputs <- inputs %>% group_by(survey_uuid) %>%
             can = mean(fd35_24a_can, na.rm=T), sa = mean(fd35_24a_sa, na.rm=T),
             npk = mean(fd35_24a_npk, na.rm=T), mrp = mean(fd35_24a_mrp, na.rm=T))
 
-iv_df <- merge(iv_df, inputs, all.x=T)
+allvars_df <- merge(allvars_df, inputs, all.x=T)
 
 # Yields - get the Z score for crop-unit
 #   ag4a_08 - Area (Acres) Farmers estimate
@@ -120,7 +120,7 @@ for (c in unique(yields$Crop.name)){
 
 yields <- yields %>% group_by(survey_uuid) %>% summarize(yield_zscore = mean(yield_zscore, na.rm=T))
 
-iv_df <- merge(iv_df, yields, all.x=T)
+allvars_df <- merge(allvars_df, yields, all.x=T)
 
 #Hired labor per Area
 labor_hired <- tbl(con, "flagging__agric_field_details") %>%
@@ -137,7 +137,7 @@ labor_hired$labor_hired <- labor_hired$ag3a_38_1 + labor_hired$ag3a_38_4 + labor
 labor_hired <- labor_hired %>% group_by(survey_uuid) %>%
   summarize(labor_hired = sum(labor_hired, na.rm=T))
 
-iv_df <- merge(iv_df, labor_hired, all.x=T)
+allvars_df <- merge(allvars_df, labor_hired, all.x=T)
 
 #Household labor per Area
 labor_hh <- tbl(con, "flagging__agric_field_details_labor") %>%
@@ -148,7 +148,7 @@ labor_hh <- tbl(con, "flagging__agric_field_details_labor") %>%
   summarize(labor_hh=sum(ag3a_70_preparing + ag3a_70_weeding +
                          ag3a_70_fertilizing + ag3a_70_harvesting, na.rm=T))
 
-iv_df <- merge(iv_df, labor_hh, all.x=T)
+allvars_df <- merge(allvars_df, labor_hh, all.x=T)
 
 #Amount Sold
 #   ag5a_01 - Did you sell any of the ${fd5_crop_name} produced
@@ -184,10 +184,22 @@ sold <- sold %>% group_by(survey_uuid) %>%
             avg_pct_crops_any_sold = mean(ag5a_01, na.rm=T)) %>%
   data.frame
 
-iv_df <- merge(iv_df, sold, all.x=T)
+allvars_df <- merge(allvars_df, sold, all.x=T)
 
 # Irrigaion
+#   ag3a_09 - Was this FIELD irrigated in the last completed
 
+irrig <- tbl(con, 'flagging__agric_field_details') %>% 
+  select(survey_uuid, flag, ag3a_09) %>%
+  data.frame %>% flagFilter
+
+irrig$ag3a_09 <- as.numeric(irrig$ag3a_09)
+irrig$ag3a_09[irrig$ag3a_09==2] <- 0
+
+irrig <- irrig %>% group_by(survey_uuid) %>%
+  summarize(pct_fields_irrigated = mean(ag3a_09, na.rm=T))
+
+allvars_df <- merge(allvars_df, irrig, all.x=T)
 
 # Inorganic Fertilizers
 #  fd33_18a_* - Select all types of organic fertilizer that you used
@@ -199,18 +211,193 @@ iv_df <- merge(iv_df, sold, all.x=T)
 #    fd33_18a_7 'Biomass Transfer', 
 #    fd33_18a_8 'Compost'
 
+###############################################
+#Get control variables
+###############################################
+
+#HH size
+size <- tbl(con, "flagging__household_secB") %>%
+  select(survey_uuid, Country, `Landscape #`, `Household ID`, flag) %>%
+  data.frame %>%
+  filter(!grepl('record is duplicated in the schema', flag)) %>%
+  group_by(Household.ID) %>%
+  summarize(hh_size=n())
+
+allvars_df <- merge(allvars_df, size, all.x=T)
+
 ##############################################
 #Get all dependant variables - Livelihoods
 ##############################################
 
 # Income
-#   Farming
-#   Labor
-# Nutrition
+#   Business Income - hh_e65_1 - income
+#   
+#   From - household_secE
+
+hhe <- tbl(con, "flagging__household_secE") %>%
+  select(hh_e65_1, hh_e65_2, #hh_e28, hh_e27, hh_e26,
+         hh_e22_1, hh_e22_2, hh_e24_1, hh_e24_2,
+         flag, `Household ID`) %>%
+  data.frame %>% flagFilter
+
+hhe$income_own <- 0
+hhe$income_own[which(hhe$hh_e65_2=='week')] <- hhe$hh_e65_1[which(hhe$hh_e65_2=='week')]*52.14286
+hhe$income_own[which(hhe$hh_e65_2=='month')] <- hhe$hh_e65_1[which(hhe$hh_e65_2=='month')]*12
+
+times <- c(1, 2, 3, 4, 5, 6, 7, 8)
+annualized <- c(8*356, 365, 52.14286, 26.07143, 12, 4, 2, 1)
+
+hhe$income_wage <- mapply(hhe$hh_e22_1*annualized[match(as.numeric(hhe$hh_e22_2), times)],
+                          hhe$hh_e24_1*annualized[match(as.numeric(hhe$hh_e24_2), times)],
+                          0, FUN=sum, na.rm=T)
+
+hhe <- hhe %>% group_by(Household.ID) %>% summarize(income_own=sum(income_own, na.rm=T),
+                                                    income_wage=sum(income_wage, na.rm=T))
+
+allvars_df <- merge(allvars_df, hhe, all.x=T)
+
+# Ag Income - (((ag10a_21+ag10a_27)-ag10a_34) + ag7a_04 + ag10b_06 + (ag09_08 - ag09_11) + (ag5a_03 + ag5a_26)) * [[exchange rate]]
+#  agric_crops_by_hh; 
+#    ag5a_03
+#    ag5a_26
+
+inc1 <- tbl(con, 'flagging__agric_crops_by_hh') %>%
+  select(`Household ID`, flag,
+         ag5a_03, ag5a_26) %>%
+  data.frame %>% flagFilter
+
+inc1$income_crops <- mapply(inc1$ag5a_03, inc1$ag5a_26, FUN=sum, na.rm=T)
+
+inc1 <- inc1 %>% group_by(Household.ID) %>% summarize(income_crops=sum(income_crops, na.rm=T))
+
+allvars_df <- merge(allvars_df, inc1, all.x=T)
+
+#  agric_perm_crop; 
+#    ag7a_04
+
+inc2 <- tbl(con, 'flagging__agric_perm_crop') %>%
+  select(`Household ID`, flag, ag7a_04) %>%
+  data.frame %>% flagFilter %>%
+  group_by(Household.ID) %>%
+  summarize(income_perm_crop=sum(ag7a_04, na.rm=T))
+
+allvars_df <- merge(allvars_df, inc2, all.x=T)
+
+#  agric_livestock_byproduct; 
+#    ag10b_06
+
+inc3 <- tbl(con, 'flagging__agric_livestock_byproduct') %>%
+  select(`Household ID`, flag, ag10b_06) %>%
+  data.frame %>% flagFilter %>%
+  group_by(Household.ID) %>%
+  summarize(income_lvstk_byprod=sum(ag10b_06, na.rm=T))
+
+allvars_df <- merge(allvars_df, inc3, all.x=T)
+
+#  agric_byproduct; 
+#    ag09_08
+#    ag09_11
+
+inc4 <- tbl(con, 'flagging__agric_byprod') %>%
+  select(`Household ID`, flag, ag09_08, ag09_11) %>%
+  data.frame %>% flagFilter
+
+inc4$ag09_11[is.na(inc4$ag09_11)] <- 0
+
+inc4$income_byprod <- inc4$ag09_08 - inc4$ag09_11
+
+inc4 <- inc4 %>% group_by(Household.ID) %>% summarize(income_byprod=sum(income_byprod, na.rm=T))
+
+allvars_df <- merge(allvars_df, inc4, all.x=T)
+
+#  agric_livestock
+#    ag10a_21
+#    ag10a_27
+#    ag10a_34
+
+inc5 <- tbl(con, 'flagging__agric_livestock') %>%
+  select(`Household ID`, flag, ag10a_21, ag10a_27, ag10a_34) %>%
+  data.frame %>% flagFilter
+
+inc5$income_lvstk <- mapply(inc5$ag10a_21, inc5$ag10a_27, FUN=sum, na.rm=T)
+
+inc5$ag10a_34[is.na(inc5$ag10a_34)] <- 0
+
+inc5$income_lvstk <- inc5$income_lvstk - inc5$ag10a_34
+
+inc5 <- inc5 %>% group_by(Household.ID) %>% summarize(income_lvstk = sum(income_lvstk, na.rm=T))
+
+allvars_df <- merge(allvars_df, inc5, all.x=T)
+
 # Ratio of Laborers to Dependants
+hh_labor <- tbl(con, 'flagging__household_secE') %>%
+  select(`Household ID`, `Individual ID`, flag, hh_e04) %>%
+  data.frame %>% flagFilter
+
+hh_labor$hh_e04 <- as.numeric(hh_labor$hh_e04)
+hh_labor$hh_e04[hh_labor$hh_e04==2] <- 0
+
+ag_labor <- tbl(con, 'flagging__agric_field_details_labor') %>%
+  select(`Household ID`, `Individual ID`, flag,
+         ag3a_70_weeding, ag3a_70_fertilizing, ag3a_70_harvesting,
+         ag3a_70_preparing) %>%
+  data.frame %>% flagFilter
+
+ag_labor[is.na(ag_labor)] <- 0
+
+ag_labor$work[which(ag_labor$ag3a_70_weeding==0 & ag_labor$ag3a_70_harvesting==0 &
+                ag_labor$ag3a_70_preparing==0 & ag_labor$ag3a_70_fertilizing==0)] <- 0
+
+ag_labor$work[which(ag_labor$ag3a_70_weeding>0 | ag_labor$ag3a_70_harvesting>0 |
+                        ag_labor$ag3a_70_preparing>0 | ag_labor$ag3a_70_fertilizing>0)] <- 1
+
+labor <- merge(hh_labor, ag_labor, all.x=F, all.y=F) %>% unique
+labor$ag_or_hh <- labor$work | labor$hh_e04
+
+labor <- labor %>% group_by(Household.ID) %>% summarize(labor_pct=mean(labor$ag_or_hh, na.rm=T))
+
+allvars_df <- merge(allvars_df, labor, all.x=T)
+
 # Education
-# Employment
+hh_ed <- tbl(con, 'flagging__household_secC') %>%
+  #filter(hh_c01!='2') %>%
+  select(`Household ID`, flag, hh_c02, hh_c03) %>%
+  data.frame %>% flagFilter
+
+hh_ed$hh_c02 <- as.numeric(hh_ed$hh_c02)
+hh_ed$hh_c03 <- as.numeric(hh_ed$hh_c03)
+
+hh_ed$hh_c02[hh_ed$hh_c02 < 5] <- 1
+hh_ed$hh_c02[hh_ed$hh_c02 == 5] <- 0
+
+hh_ed$hh_c03[hh_ed$hh_c03==2] <- 0
+
+hh_ed <- hh_ed %>% group_by(Household.ID) %>% summarize(any_ed_perc=mean(hh_c03, na.rm=T),
+                                                        literate_perc=mean(hh_c02, na.rm=T))
+
+allvars_df <- merge(allvars_df, hh_ed, na.rm=T)
+
+# Employment Ratio
+hh_employ <- tbl(con, 'flagging__household_secE') %>%
+  select(`Household ID`, flag, hh_e08) %>%
+  data.frame %>% flagFilter
+
+hh_employ$hh_e08 <- as.numeric(hh_employ$hh_e08)
+hh_employ$hh_e08[hh_employ$hh_e08==2] <- 0
+
+hh_employ <- hh_employ %>% group_by(Household.ID) %>%
+  summarize(employ_pct=mean(hh_e08, na.rm=T))
+
+allvars_df <- merge(allvars_df, hh_employ, all.x=T)
+
 # Nutrition
+nutr <- tbl(con, 'indicators__nutrition') %>%
+  group_by(`Household ID`) %>%
+  summarize(mean_len_z=mean(zlen, na.rm=T),
+            mean_wei_z=mean(zwei, na.rm=T),
+            mean_wfl_z=mean(zwfl, na.rm=T))
+
+allvars_df <- merge(allvars_df, nutr, all.x=T)
 
 ######################################
 #Combine and Analyze
@@ -231,7 +418,10 @@ iv_df <- merge(iv_df, sold, all.x=T)
 
 #lots of missing data in the labor area, lots of 0's and NAs.
 
-#BIG data quality issue: 0s vs NAs
+#BIG data quality issue: 0s vs NAs.  Need to address this!
 
 #We've removed flagged outliers.  But if they are real values and not outliers,
 #  then we are missing vital info.
+
+#dealing with seasons and fields - right now we treat one field, two seasons as two fields
+
