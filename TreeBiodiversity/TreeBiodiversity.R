99402dc2ddf9e13362742b0212995b335b01be39
mcooperci <mw.coop.r@gmail.com>
2017-03-01 15:49:56 +0200

    Add Rwanda

--- TreeBiodiversity/TreeBiodiversity.R
+++ TreeBiodiversity/TreeBiodiversity.R
@@ -45,7 +45,7 @@ bootstrap_diversity <- function(vector, freq, iter){
 }
 
 #The area of the plots are not the same, so we have to somehow adjust that.  Restrict to smallest plot size?
-eplot <- tbl(con, 'flagging__eplot') %>% select(survey_uuid, latitude, longitude, `Subplot Radius`, flag) %>% data.frame %>%
+eplot <- tbl(con, 'flagging__eplot') %>% select(survey_uuid, Country, latitude, longitude, `Subplot Radius`, flag) %>% data.frame %>%
   filter(!grepl('Subplot Radius', flag) & !is.na(Subplot.Radius))
 
 radii <- unique(eplot$Subplot.Radius)
@@ -56,34 +56,36 @@ subplot <- data.frame(Subplot.Radius=radii, area, ratio)
 
 eplot <- merge(eplot, subplot)
 
-species <- tbl(con, 'flagging__eplot_woody_plant') %>% data.frame
+species <- tbl(con, 'flagging__eplot_woody_plant') %>% select(survey_uuid, Country, Genus, Species) %>% data.frame
 
 species <- merge(eplot, species)
 
-species_sum <- species %>% group_by(survey_uuid, Landscape.., Eplot.., Country, latitude, longitude) %>% 
+species <- species %>% filter(Genus != 'Musa')
+
+species_sum <- species %>% group_by(survey_uuid, latitude, longitude, Country) %>% 
   summarize(biodiversity=bootstrap_diversity(vector=paste0(Genus, Species), freq=ratio, iter=500))
 
-library(maps)
-library(mapdata)
+# library(maps)
+# library(mapdata)
 
-color.gradient <- function(x, colors=c("red","yellow","green"), colsteps=100) {
-  return( colorRampPalette(colors) (colsteps) [ findInterval(x, seq(min(x),max(x), length.out=colsteps)) ] )
-}
-
-species_sum$color <- color.gradient(species_sum$biodiversity)
-
-colfunc<-colorRampPalette(c("red","yellow","springgreen","royalblue"))
-
-plot(species_sum$longitude, species_sum$latitude, col=species_sum$color, 
-     xlim=c(30,40), ylim=c(-10.5,-6), xlab='', ylab='', axes = F)
-map('worldHires', 'Tanzania', add=T)
+# color.gradient <- function(x, colors=c("red","yellow","green"), colsteps=100) {
+#   return( colorRampPalette(colors) (colsteps) [ findInterval(x, seq(min(x),max(x), length.out=colsteps)) ] )
+# }
+# 
+# species_sum$color <- color.gradient(species_sum$biodiversity)
+# 
+# colfunc<-colorRampPalette(c("red","yellow","springgreen","royalblue"))
+# 
+# plot(species_sum$longitude, species_sum$latitude, col=species_sum$color, 
+#      xlim=c(30,40), ylim=c(-10.5,-6), xlab='', ylab='', axes = F)
+# map('worldHires', 'Tanzania', add=T)
 
 library(sp)
 library(gstat)
 library(raster)
 
 species_sum <- as.data.frame(species_sum)
-ss <- SpatialPointsDataFrame(coords=species_sum[species_sum$Country=='TZA' , c('longitude', 'latitude')], data=species_sum[species_sum$Country=='TZA', ])
+ss <- SpatialPointsDataFrame(coords=species_sum[species_sum$Country=='RWA' , c('longitude', 'latitude')], data=species_sum[species_sum$Country=='RWA', ])
 
 x.range <- range(ss$longitude)
 y.range <- range(ss$latitude)
@@ -98,19 +100,27 @@ gridded(grd) <- TRUE
 proj4string(ss) <- CRS("+proj=longlat +datum=WGS84")
 proj4string(grd) <- CRS("+proj=longlat +datum=WGS84")
 
-IDW <- gstat::idw(biodiversity~1, locations=ss, newdata=grd, idp=1.0)
-plot(IDW)
+write.csv(ss@data, 'RWA-Biodiversity.csv', row.names=F)
 
-TZ <- readRDS('TZA_adm0.rds')
+IDW <- gstat::idw(biodiversity~1, locations=ss, newdata=grd, idp=2.5)
+
+#plot(IDW)
+
+# TZ <- readRDS('UGA_adm1.rds')
+# TZ <- TZ[TZ$NAME_1!="Lake Victoria", ]
+
+TZ <- readRDS('RWA_adm0.rds')
+
 
 cp <- mask(raster(IDW), TZ)
 
-plot(cp, main='Biodiversity in Southern Tanzania', labels=F, xaxt='n', yaxt='n',
+library(raster)
+plot(cp, main='Tree Species Diversity in Rwanda', labels=F, xaxt='n', yaxt='n',
      legend.args=list(text='Tree Biodiversity (Shannon Diversity Index)', side=4, font=2, line=2.5, cex=0.8))
+plot(TZ, add=T)
 
-points(38.7492089681939,	-8.14419444155308, pch=0, col='red', cex=1.5)
-
-legend(30.6, -9.62, 'Rufiji Landscape', pch=0, col='red', cex=1.5)
+#points(38.7492089681939,	-8.14419444155308, pch=0, col='red', cex=1.5)
+#legend(30.6, -9.62, 'Rufiji Landscape', pch=0, col='red', cex=1.5)
 
 ############################
 #Bring in landcover type
