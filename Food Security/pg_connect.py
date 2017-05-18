cd390546a3bcf4b31a23b5b0654ddeaad6553711
Matt Cooper <mcooper@conservation.org>
2017-05-18 17:52:10 -0400

    extract data within buffer

--- Food Security/pg_connect.py
+++ Food Security/pg_connect.py
@@ -1,5 +1,5 @@
 import psycopg2
-import os
+import ee
 
 execfile('D:/Documents and Settings/mcooper/GitHub/vitalsigns-analysis/production_connection.py')
 
@@ -11,10 +11,14 @@ cur.execute("SELECT uuid, gpsse_lat, gpsse_long, end_date FROM household_pii")
 
 out = cur.fetchall()
 
-for row in out:
-    point = ee.Geometry.Point(row[1], row[2]).buffer(10000)
-    res = point.area()
-    
-import ee
+ee.Initialize()
 
-image = ee.Image('srtm90_v4')
+image = ee.Image('UMD/hansen/global_forest_change_2015')
+image = image.select('last_b30')
+
+for row in out:
+    point = ee.Geometry.Point(row[1], row[2]).buffer(1000)
+
+    res = image.reduceRegion(reducer=ee.Reducer.histogram(), 
+                       geometry=point).getInfo()
+
